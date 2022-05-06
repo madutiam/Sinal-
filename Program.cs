da4a85cb4043b62c9077ac74100eb943fcc6990b
Maria Eduarda Bedetti <100002186+madutiam@users.noreply.github.com>
2022-05-06 20:27:20 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿int numeroDigitado;
+
+Console.Write("Digite um número: ");
+numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+if (numeroDigitado == 0)
+{
+    Console.WriteLine("Zero");
+}
+else
+{
+    if (numeroDigitado < 0)
+    {
+        Console.WriteLine("Negativo");
+    }
+    else
+    {
+        Console.WriteLine("positivo");
+    }
+}
